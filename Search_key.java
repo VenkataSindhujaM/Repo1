414cc2eef1af4d5bfa0a794695fd44daa7989513
VenkataSindhujaM <sindhu.mareedu2@gmail.com>
2024-06-18 20:22:28 +0530

    Key version2

--- Search_key.java
+++ Search_key.java
@@ -2,10 +2,23 @@ import java.util.*;
 public class Search_key{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
+        System.out.print("n:");
         int n=sc.nextInt();
         int a[]=new int[n];
+        System.out.print("k:");
+        int k=sc.nextInt();
+        int flag=0,ind=0;
+        System.out.println("Elements:");
         for(int i=0;i<n;i++){
             a[i]=sc.nextInt();
+            if(a[i]==k){
+                flag=1;
+                ind=i;
+            }
         }
+        if(flag==0)
+        System.out.println("Not Found");
+        else
+        System.out.println("Found at index : "+ind);
     }
 }
