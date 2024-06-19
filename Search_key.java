84bead0d0e6bc582a734489998985989ed77b934
VenkataSindhujaM <sindhu.mareedu2@gmail.com>
2024-06-19 10:54:30 +0530

    From Copy File

--- Search_key.java
+++ Search_key.java
@@ -17,7 +17,7 @@ public class Search_key{
             }
         }
         if(flag==0)
-        System.out.println("Key Not Found");
+        System.out.println("Key Not Found at any index");
         else
         System.out.println("Found at  a index : "+ind);
         System.out.println("Found at the index : "+ind);
